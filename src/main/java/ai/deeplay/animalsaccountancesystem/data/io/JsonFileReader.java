2873b3c40ea96fdd859c2c62c2da83b1bcd44329
Kiselev Danil <dabikis2855@gmail.com>
2024-04-22 21:59:42 +0600

    [+] add file reader

---
+++ src/main/java/ai/deeplay/animalsaccountancesystem/data/io/JsonFileReader.java
@@ -0,0 +1,41 @@
+package ai.deeplay.animalsaccountancesystem.data.io;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * WARNING! May cause memory problem caused by String max length;
+ * */
+@Setter
+@Getter
+public class JsonFileReader implements IDataReader {
+
+    private Path filePath;
+
+
+    @Override
+    public String read() {
+        if (filePath == null) {
+            throw new RuntimeException("Wrong filePath parameter");
+        }
+        List<String> list;
+
+        try {
+            list = Files.readAllLines(filePath);
+        }
+        catch(IOException exception) {
+            throw new RuntimeException("IO Exception occurred");
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (String str : list ){
+            sb.append(str);
+        }
+        return sb.toString();
+    }
+}
